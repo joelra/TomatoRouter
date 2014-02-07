8030dc3ca615e248047f796e45028c6919b09109
Joel <joelra@marriottschool.byu.edu>
2014-02-07 12:11:37 -0700

    Initial commit

---
+++ tomato/src/main/java/com/somethingprofane/tomato/Parser.java
@@ -0,0 +1,11 @@
+package com.somethingprofane.tomato;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/**
+ * Created by somethingPr0fane on 2/6/14.
+ */
+public class Parser {
+    Document doc = null;
+}
