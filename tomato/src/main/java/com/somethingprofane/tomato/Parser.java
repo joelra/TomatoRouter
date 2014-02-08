ccffc6f432e10b379b0092631880336c58ccac93
Joel <joelra@marriottschool.byu.edu>
2014-02-08 08:24:46 -0700

    Parser class information.

--- tomato/src/main/java/com/somethingprofane/tomato/Parser.java
+++ tomato/src/main/java/com/somethingprofane/tomato/Parser.java
@@ -2,10 +2,39 @@ package com.somethingprofane.tomato;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
 
 /**
  * Created by somethingPr0fane on 2/6/14.
  */
 public class Parser {
-    Document doc = null;
+
+    public static void main(String[] args){
+
+    }
+
+    public void parse() {
+        Document doc = null;
+        try{
+            // Get the HTML
+            doc = Jsoup.connect("http://google.com").userAgent("Mozilla").get();
+
+            String title = doc.title();
+            System.out.println("Title: " + title);
+
+            Elements elementArray;
+            elementArray = doc.getAllElements();
+
+            for(Element element : elementArray){
+                System.out.println(element.text());
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
