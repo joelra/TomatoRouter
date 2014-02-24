fbf401f57f7ff6767c11947a8722cd68842ad2d7
Joel <joelra@marriottschool.byu.edu>
2014-02-24 16:18:28 -0700

    Had to update the strings

--- tomato/src/main/java/com/somethingprofane/tomato/MainLoginActivity.java
+++ tomato/src/main/java/com/somethingprofane/tomato/MainLoginActivity.java
@@ -83,7 +83,7 @@ public class MainLoginActivity extends ActionBarActivity {
 
         protected void onPostExecute(String response) {
             // access the activity thread
-            if(responseCode == 200){
+            if(response == "200"){
                 Intent intent = new Intent(MainLoginActivity.this, OpenNewPageActivity.class);
                 MainLoginActivity.this.startActivity(intent);
             } else {
