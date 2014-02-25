72b86fe63b629c2a1c583da9ae24833158a65555
Joel <joelra@marriottschool.byu.edu>
2014-02-25 11:44:33 -0700

    Added in router and device class so that we can have that information.

---
+++ tomato/src/main/java/com/somethingprofane/tomato/Device.java
@@ -0,0 +1,15 @@
+package com.somethingprofane.tomato;
+
+/**
+ * Created by somethingPr0fane on 2/25/14.
+ */
+public class Device {
+    /**
+     * Contains all the information about a connected deivce to the router:
+     * MAC address
+     * Name
+     * IP address assigned
+     * How long connected
+     * etc.
+     */
+}
---
+++ tomato/src/main/java/com/somethingprofane/tomato/Router.java
@@ -0,0 +1,14 @@
+package com.somethingprofane.tomato;
+
+/**
+ * Created by somethingPr0fane on 2/25/14.
+ */
+public class Router {
+    /**
+     * This class with hold all the informaiton about the router:
+     * IP address
+     * MAC Address
+     * Devices Connected
+     * etc.
+     */
+}
