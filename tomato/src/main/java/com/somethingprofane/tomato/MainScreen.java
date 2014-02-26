ba1907cd750385f565b43f061e914cb0d71dc889
ghendrickson89 <garrett.hendrickson@gmail.com>
2014-02-26 06:46:32 -0800

    Adding some to router class and tieing to a view

--- tomato/src/main/java/com/somethingprofane/tomato/MainScreen.java
+++ tomato/src/main/java/com/somethingprofane/tomato/MainScreen.java
@@ -1,16 +1,28 @@
 package com.somethingprofane.tomato;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainScreen extends ActionBarActivity {
 
+    @InjectView(R.id.mainscr_btnRouter) Button routerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        ButterKnife.inject(this);
+
     }
 
 
@@ -32,6 +44,23 @@ public class MainScreen extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.mainscr_btnRouter)
+    public void LoginClicked(Button routerButton){
+        new moveToRouter().execute();
+    }
+
+    private class moveToRouter extends AsyncTask<TextView, Void, String> {
+
+        @Override
+        protected String doInBackground(TextView... textViews) {
+            Intent intent = new Intent(MainScreen.this, RouterOverviewActivity.class);
+            MainScreen.this.startActivity(intent);
+            // Finish the activity;
+            finish();
+            return null;
+        }
     }
 
 }
--- tomato/src/main/java/com/somethingprofane/tomato/RouterOverviewActivity.java
+++ tomato/src/main/java/com/somethingprofane/tomato/RouterOverviewActivity.java
@@ -17,14 +17,14 @@ import butterknife.OnClick;
 /**
  * Created by garrett on 2/25/14.
  */
-public class RouterViewActivity extends Activity {
+public class RouterOverviewActivity extends Activity {
 
 
-    @InjectView(R.id.mainscr_btnRouter) Button routerButton;
+    @InjectView(R.id.router_overview_retrieveRouter) Button routerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
+        setContentView(R.layout.activity_router_overview);
 
         ButterKnife.inject(this);
 
@@ -36,6 +36,7 @@ public class RouterViewActivity extends Activity {
 
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_screen, menu);
+        new retrieveRouterInfo().execute();
         return true;
     }
 
@@ -51,17 +52,27 @@ public class RouterViewActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    @OnClick(R.id.mainscr_btnRouter)
-    public void LoginClicked(Button routerButton){
-        new retrieveRouterInfo().execute();
-    }
+    //@OnClick(R.id.router_overview_retrieveRouter)
+   // public void LoginClicked(Button routerButton){
+//
+  //  }
 
     private class retrieveRouterInfo extends AsyncTask<TextView, Void, String> {
+
+        TextView routerName = (TextView) findViewById(R.id.router_main_labelRouter);
 
         @Override
         protected String doInBackground(TextView... textViews) {
 
-            new Router("http://192.168.1.1","root","admin");
+            Router tempRouter = new Router("http://192.168.1.1","root","admin");
+            String currentText = routerName.getText().toString();
+            try{
+                routerName.append(" "+tempRouter.getRouterName());
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
 
             return null;
         }
