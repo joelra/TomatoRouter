e4e7add754b0c4027a8ea0ee507851b4b83afd68
Brian Black <brian.black@domo.com>
2014-03-01 13:20:02 -0700

    added basic config and admin config plus layout files

---
+++ tomato/src/main/java/com/somethingprofane/tomato/AdministrationConfiguration.java
@@ -0,0 +1,17 @@
+package com.somethingprofane.tomato;
+
+/**
+ * Created by Brian Black on 3/1/14.
+ */
+public class AdministrationConfiguration {
+
+    /**
+     * This administration page is meant to allow the user to
+     * change the router name, username, and password as well
+     * as access settings
+     */
+
+
+
+
+}
---
+++ tomato/src/main/java/com/somethingprofane/tomato/BasicConfiguration.java
@@ -0,0 +1,50 @@
+package com.somethingprofane.tomato;
+
+/**
+ * Created by Brian Black on 3/1/14.
+ */
+
+import android.app.ActionBar;
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+
+public class BasicConfiguration extends ActionBarActivity {
+
+    /**
+     * This class will include editable username/password, change SSID
+     * set static DNS, change DHCP server lease time and ip address range,
+     * assigning a static ip to a MAC address, and possibly port forwarding
+     */
+
+
+
+
+
+
+
+
+}
