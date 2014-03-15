176d30aa3b475bdb0ab207ce9893b40d529b1be3
Joel <joelra@marriottschool.byu.edu>
2014-03-15 15:47:26 -0600

    Updated the main screen activity so that it works with the activity.

--- tomato/src/main/java/com/somethingprofane/tomato/MainScreen.java
+++ tomato/src/main/java/com/somethingprofane/tomato/MainScreen.java
@@ -8,7 +8,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -23,7 +22,7 @@ public class MainScreen extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_main);
+        setContentView(R.layout.activity_main_screen);
 
         ButterKnife.inject(this);
 
