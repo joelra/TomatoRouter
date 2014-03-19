30e12ec8df32e3810b162d4ec02e9706b2152e80
Joel <joelra@marriottschool.byu.edu>
2014-03-18 23:20:31 -0600

    Updated the getView method. Need to make good icons to display on each row.

--- tomato/src/main/java/com/somethingprofane/tomato/DeviceListBaseAdapter.java
+++ tomato/src/main/java/com/somethingprofane/tomato/DeviceListBaseAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -13,7 +14,7 @@ import java.util.List;
 /**
  * Created by somethingPr0fane on 3/18/14.
  */
-public class DeviceListBaseAdapter {
+public class DeviceListBaseAdapter extends BaseAdapter {
 
     private Context context;
     private List<Device> deviceList;
@@ -28,6 +29,19 @@ public class DeviceListBaseAdapter {
         TextView txtDeviceName;
         TextView txtDeviceIP;
         ImageView imgDeviceIcon;
+        int position;
+        View root;
+
+        public ViewHolder(int postition, View view){
+            super();
+            this.position = position;
+            this.root = view;
+            setView();
+        }
+
+        private void setView() {
+            // TODO: place in an onclick listner on the WiFi toggle that will turn the wifi on and off.
+        }
     }
 
     public View getView(int position, View convertView, ViewGroup parent){
@@ -36,8 +50,34 @@ public class DeviceListBaseAdapter {
         if(convertView == null) {
 //            convertView = inflater.inflate(R.layout.list_item, null);
 //            holder = new ViewHolder(position, convertView);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
+
+        Device device = (Device) getItem(position);
+        // Set the info for each row:
+        holder.txtDeviceName.setText(device.getDeviceName());
+        holder.txtDeviceIP.setText(device.getDeviceIPAddr());
+
+        //TODO Check to see if the device is currently connected via wifi and display the appropriate icon
 
         return convertView;
     }
+
+    @Override
+    public int getCount(){
+        return deviceList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return deviceList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return deviceList.indexOf(getItem(i));
+    }
+
+
 }
