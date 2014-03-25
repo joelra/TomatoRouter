35a15c27a9360b50e12a5685098fa22094a21437
Joel <joelra@marriottschool.byu.edu>
2014-03-24 21:15:33 -0600

    Created background gradient to highlight selected item in ListView on DeviceScreen

--- tomato/src/main/java/com/somethingprofane/tomato/DeviceListBaseAdapter.java
+++ tomato/src/main/java/com/somethingprofane/tomato/DeviceListBaseAdapter.java
@@ -1,10 +1,14 @@
 package com.somethingprofane.tomato;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -19,6 +23,7 @@ public class DeviceListBaseAdapter extends BaseAdapter {
 
     private Context context;
     private ArrayList<Device> deviceList;
+    private AlertDialog alert;
 
     public DeviceListBaseAdapter(Context context, ArrayList<Device> devices){
         this.context = context;
@@ -30,11 +35,15 @@ public class DeviceListBaseAdapter extends BaseAdapter {
         TextView txtDeviceName;
         TextView txtDeviceIP;
         ImageView imgDeviceIcon;
+        TextView dialog_name;
+        TextView dialog_ip;
+        Button dialog_ok;
+        Button dialog_cancel;
     }
 
     public View getView(int position, View convertView, ViewGroup parent){
         ViewHolder holder = null;
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if(convertView == null) {
             convertView = inflater.inflate(R.layout.list_item_device, null);
             holder = new ViewHolder();
@@ -47,7 +56,7 @@ public class DeviceListBaseAdapter extends BaseAdapter {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        Device device = (Device) getItem(position);
+        final Device device = (Device) getItem(position);
         // Set the info for each row:
         holder.txtDeviceName.setText(device.getDeviceName());
         holder.txtDeviceIP.setText(device.getDeviceIPAddr());
@@ -57,15 +66,54 @@ public class DeviceListBaseAdapter extends BaseAdapter {
         //TODO Check to see if the device is currently connected via wifi and display the appropriate icon
         if(device.getDeviceType() == "wireless") {
             if(device.isDeviceWifiConnected()){
-                holder.imgDeviceIcon.setImageResource(R.drawable.devices_wifi_on);
+                holder.imgDeviceIcon.setBackgroundResource(R.drawable.devices_wifi_on);
             }else {
-                holder.imgDeviceIcon.setImageResource(R.drawable.devices_wifi_off);
+                holder.imgDeviceIcon.setBackgroundResource(R.drawable.devices_wifi_off);
             }
         }else {
-            holder.imgDeviceIcon.setImageResource(R.drawable.device_list_icon);
+            holder.imgDeviceIcon.setBackgroundResource(R.drawable.device_list_icon);
         }
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ViewHolder holder = new ViewHolder();
+                AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+                View dialogView = inflater.inflate(R.layout.dialog_device, null);
+                dialog.setTitle("Device Information");
+                holder.dialog_name = (TextView) dialogView.findViewById(R.id.dialog_device_txtName);
+                holder.dialog_ip = (TextView) dialogView.findViewById(R.id.dialog_device_txtIPAddress);
+                holder.dialog_ok = (Button) dialogView.findViewById(R.id.dialog_device_btnOK);
+                holder.dialog_cancel = (Button) dialogView.findViewById(R.id.dialog_device_btnCancel);
+
+                holder.dialog_name.setText(device.getDeviceName());
+                holder.dialog_ip.setText(device.getDeviceIPAddr());
+                holder.dialog_ok.setOnClickListener(dialogDevice_UpdateOnClickListener);
+                holder.dialog_cancel.setOnClickListener(dialogDevice_DismissOnClickListener);
+
+                dialog.setView(dialogView);
+                alert = dialog.create();
+                alert.show();
+            }
+        });
+
         return convertView;
     }
+
+    private Button.OnClickListener dialogDevice_UpdateOnClickListener = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            //Todo Change the group that the device is currently in.
+        }
+    };
+    private Button.OnClickListener dialogDevice_DismissOnClickListener = new Button.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            alert.dismiss();
+        }
+    };
 
     @Override
     public int getCount(){
--- tomato/src/main/java/com/somethingprofane/tomato/DeviceScreen.java
+++ tomato/src/main/java/com/somethingprofane/tomato/DeviceScreen.java
@@ -9,6 +9,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
